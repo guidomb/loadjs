544255df807c027645a366887af035f3d2981523
Guido Marucci Blas <guidomb@gmail.com>
2013-04-19 14:46:57 -0300

    Fixes #1 by adding multiple controller syntax

--- vendor/assets/javascripts/loadjs.js
+++ vendor/assets/javascripts/loadjs.js
@@ -1,11 +1,30 @@
 function load(options, fn) {
   $(function(){
-    if (options.controller != $("body").data("controller")) {
-      return;
-    }
-    if (typeof options.action === "undefined" ||
-      options.action == $("body").data("action")) {
-      fn();
+    var body = $("body");
+    var currentController = body.data("controller");
+    var currentAction = body.data("action");
+
+    if (typeof options.controllers !== "undefined") {
+      for (var controller in options.controllers) {
+        var actions = options.controllers[controller];
+        if (controller == currentController && actions.length == 0) {
+          fn();
+        } else {
+          for (var i = 0, length = actions.length; i < length; ++i) {
+            if (actions[i] == currentAction) {
+              fn();
+              break;
+            }
+          }
+        }
+      }
+    } else {
+      if (options.controller != currentController) {
+        return;
+      }
+      if (typeof options.action === "undefined" || options.action == currentAction) {
+        fn();
+      }
     }
   });
 }
