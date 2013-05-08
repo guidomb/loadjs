5278c9f60e69df73245d8e338f9eae5de4787a5a
Amadeus Folego <amadeusfolego@gmail.com>
2013-05-07 23:22:16 -0300

    Avoids same controller or action loadings

--- spec/javascripts/LoadJSSpec.js
+++ spec/javascripts/LoadJSSpec.js
@@ -3,11 +3,26 @@ describe("#load", function() {
     expect(load).toBeDefined();
   });
 
-  it("loads the string 'projects#new'", function() {
-    loadFunction = jasmine.createSpy('loadFunction');
-    $('body').data('controller', 'projects')
-             .data('action', 'new');
-    load("projects#new", loadFunction);
-    expect(loadFunction).toHaveBeenCalledWith("projects", "new");
+  describe("when the body has data-controller=projects and data-action=new", function() {
+    beforeEach(function() {
+      loadFunction = jasmine.createSpy('loadFunction');
+      $('body').data('controller', 'projects')
+               .data('action', 'new');
+    });
+
+    it("loads the string 'projects#new'", function() {
+      load("projects#new", loadFunction);
+      expect(loadFunction).toHaveBeenCalledWith("projects", "new");
+    });
+
+    it("does not load with different action but same controller", function() {
+      load("projects#edit", loadFunction);
+      expect(loadFunction).not.toHaveBeenCalled();
+    });
+
+    it("does not load with different controller but same action", function() {
+      load("pages#new", loadFunction);
+      expect(loadFunction).not.toHaveBeenCalled();
+    });
   });
 });
