4ebb0eadc49521eb4d8fde2539b777bbc65ea2dc
Amadeus Folego <amadeusfolego@gmail.com>
2013-05-07 23:13:23 -0300

    Spec 'controller#action'

--- spec/javascripts/LoadJSSpec.js
+++ spec/javascripts/LoadJSSpec.js
@@ -2,4 +2,12 @@ describe("#load", function() {
   it("is defined", function() {
     expect(load).toBeDefined();
   });
+
+  it("loads the string 'projects#new'", function() {
+    loadFunction = jasmine.createSpy('loadFunction');
+    $('body').data('controller', 'projects')
+             .data('action', 'new');
+    load("projects#new", loadFunction);
+    expect(loadFunction).toHaveBeenCalledWith("projects", "new");
+  });
 });
